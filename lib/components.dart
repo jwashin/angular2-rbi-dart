6bffa3e74d5eb11169fe0368225a602f0e0d15a1
Jim Washington <washington.jim@gmail.com>
2016-03-03 08:56:43 -0500

    added Textfield to components list

--- lib/components.dart
+++ lib/components.dart
@@ -5,6 +5,7 @@ import 'src/components/switch.dart';
 import 'src/components/icontoggle.dart';
 import 'src/components/radio.dart';
 import 'src/components/input_directives.dart';
+import 'src/components/textfield.dart';
 
 export 'src/components/button.dart';
 export 'src/components/checkbox.dart';
@@ -13,6 +14,7 @@ export 'src/components/switch.dart';
 export 'src/components/icontoggle.dart';
 export 'src/components/radio.dart';
 export 'src/components/input_directives.dart';
+export 'src/components/textfield.dart';
 
 const List<Type> rbiComponents = const [
   Button,
@@ -21,7 +23,11 @@ const List<Type> rbiComponents = const [
   FocusSource,
   Checkbox,
   Slider,
+  SliderBackgroundUpper,
+  SliderBackgroundLower,
+  SliderElement,
   Switch,
   IconToggle,
-  Radio
+  Radio,
+  Textfield
 ];
