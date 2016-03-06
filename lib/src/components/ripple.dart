b572f6a5602b755260db0019d49273d4b96de04c
Jim Washington <washington.jim@gmail.com>
2016-03-06 07:54:54 -0500

    removed intermediate variables and unnecessary class

--- lib/src/components/ripple.dart
+++ lib/src/components/ripple.dart
@@ -4,12 +4,6 @@ import 'dart:html';
 import 'dart:math' show sqrt;
 import 'package:angular2/angular2.dart';
 
-@Directive(selector: '.mdl-ripple')
-class Ripple {
-  @HostBinding('class.is-visible') bool visible = false;
-  @HostBinding('class.is-animating') bool animating = false;
-}
-
 @Component(
     selector: '.mdl-button__ripple-container, '
         '.mdl-menu__item-ripple-container, '
@@ -18,52 +12,39 @@ class Ripple {
         '.mdl-radio__ripple-container, '
         '.mdl-icon-toggle__ripple-container',
     template: '<span class="mdl-ripple ng-animate" *ngIf="active"'
-        '[style.width]="rippleWidth" '
-        '[style.height]="rippleHeight" '
+        '[style.width]="rippleSize" '
+        '[style.height]="rippleSize" '
         '[style.top]="rippleY" '
         '[style.left]="rippleX" '
         '></span>',
     styles: const [
       '.mdl-ripple {transform: translate(-50%, -50%) scale(1);'
-          'transition: all 0.6s cubic-bezier(0.0, 0.0, 0.2, 1.0);}',
+          'transition: all 0.6s cubic-bezier(0, 0, 0.2, 1);}',
       '.mdl-ripple.ng-enter {opacity: 0;'
           'transform: translate(-50%, -50%) scale(0.0001);}',
       '.mdl-ripple.ng-enter-active {opacity: .8;'
           'transform: translate(-50%, -50%) scale(1);}'
     ],
     directives: const [
-      Ripple,
       NgIf
     ])
 class RippleContainer {
-  @ViewChild(Ripple) Ripple ripple;
   @Input() bool centered = false;
   bool active = false;
-  String rippleX, rippleY, rippleWidth, rippleHeight;
+  String rippleX, rippleY, rippleSize;
 
   @HostListener('mousedown', const ['\$event.client', '\$event.target'])
-  void onMouseDown(Point client, Element ripple) {
-    Rectangle rect = ripple.getBoundingClientRect();
-    int boundHeight = rect.height.toInt();
-    int boundWidth = rect.width.toInt();
-    int rippleSize =
-    (sqrt(boundWidth * boundWidth + boundHeight * boundHeight) * 2 + 2)
-        .toInt();
-    rippleWidth = '${rippleSize}px';
-    rippleHeight = '${rippleSize}px';
-    num x, y;
-    num clientX, clientY;
-    clientX = client.x;
-    clientY = client.y;
+  void onMouseDown(Point clickPoint, Element rippleContainer) {
+    Rectangle rect = rippleContainer.getBoundingClientRect();
+    rippleSize = '${
+        sqrt(rect.width * rect.width + rect.height * rect.height) * 2 + 2}px';
     if (centered) {
-      x = (rect.right - rect.left) / 2;
-      y = (rect.bottom - rect.top) / 2;
+      rippleX = '${(rect.right - rect.left) / 2}px';
+      rippleY = '${(rect.bottom - rect.top) / 2}px';
     } else {
-      y = clientY - rect.top;
-      x = clientX - rect.left;
+      rippleY = '${clickPoint.y - rect.top}px';
+      rippleX = '${clickPoint.x - rect.left}px';
     }
-    rippleX = '${x}px';
-    rippleY = '${y}px';
     active = true;
   }
 
@@ -76,5 +57,3 @@ class RippleContainer {
   void onTouchStart(Point client, Element target) =>
       onMouseDown(client, target);
 }
-
-const List ripples = const [Ripple, RippleContainer];
