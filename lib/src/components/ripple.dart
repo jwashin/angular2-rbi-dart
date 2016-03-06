5a7af3ed33850fd498202efbfd8f6a24360ff1a5
Jim Washington <washington.jim@gmail.com>
2016-03-06 06:36:34 -0500

    ripple now using ng-animate

--- lib/src/components/ripple.dart
+++ lib/src/components/ripple.dart
@@ -2,121 +2,79 @@ library component_ripple;
 
 import 'dart:html';
 import 'dart:math' show sqrt;
-import 'dart:async';
-import 'package:angular2_rbi/src/util/animation_frame.dart'
-    show getAnimationFrame;
 import 'package:angular2/angular2.dart';
-
-const String initialScale = 'scale(0.0001, 0.0001)';
-const String finalScale = '';
 
 @Directive(selector: '.mdl-ripple')
 class Ripple {
   @HostBinding('class.is-visible') bool visible = false;
   @HostBinding('class.is-animating') bool animating = false;
-  @HostBinding('style.webkitTransform') String transformStringWK;
-  @HostBinding('style.msTransform') String transformStringMS;
-  @HostBinding('style.transform') String transformString;
-
-  @HostBinding('style.width') String width = '';
-  @HostBinding('style.height') String height = '';
-
-  @Input() bool centered = false;
-
-  int frameCount = 0;
-  int boundWidth;
-  int boundHeight;
-  int x;
-  int y;
-
-  Future doRipple(int clientX, int clientY, Element target) async {
-//    centered = center;
-    if (width.isEmpty || height.isEmpty) {
-      Rectangle rect = target.getBoundingClientRect();
-      boundHeight = rect.height.toInt();
-      boundWidth = rect.width.toInt();
-      int rippleSize =
-      (sqrt(boundWidth * boundWidth + boundHeight * boundHeight) * 2 + 2)
-          .toInt();
-      width = '${rippleSize}px';
-      height = '${rippleSize}px';
-    }
-    visible = true;
-    if (frameCount > 0) return;
-    frameCount = 1;
-    Rectangle bound = target.getBoundingClientRect();
-    x = clientX == null ? bound.right : (clientX - bound.left).round();
-    y = clientY == null ? bound.bottom : (clientY - bound.top).round();
-//    y = (clientY - bound.top).round();
-    setTransformString(true);
-    await getAnimationFrame();
-    animationFrameHandler();
-  }
-
-  Future animationFrameHandler() async {
-    if (frameCount-- > 0) {
-      await getAnimationFrame();
-      animationFrameHandler();
-    } else {
-      setTransformString(false);
-    }
-  }
-
-//'translate(-50%, -50%) $offset $scale'
-  void setTransformString(bool start) {
-    String scale = start ? initialScale : finalScale;
-    String offset = centered && start
-        ? 'translate(${boundWidth / 2}px, ${boundHeight / 2}px)'
-        : 'translate(${x}px, ${y}px)';
-    transformString = 'translate(-50%, -50%) $offset $scale';
-    transformStringMS = transformString;
-    transformStringWK = transformString;
-    animating = !start;
-  }
 }
 
 @Component(
-    selector: '.mdl-switch__ripple-container,'
-        '.mdl-checkbox__ripple-container,'
-        '.mdl-radio__ripple-container,'
+    selector: '.mdl-button__ripple-container, '
+        '.mdl-menu__item-ripple-container, '
+        '.mdl-switch__ripple-container, '
+        '.mdl-checkbox__ripple-container, '
+        '.mdl-radio__ripple-container, '
         '.mdl-icon-toggle__ripple-container',
-    template: '<span class="mdl-ripple" [centered]="true"></span>',
-    directives: const [Ripple])
-class CenteredRippleContainer {
-  @ViewChild(Ripple) Ripple ripple;
-
-  @HostListener('mousedown', const ['\$event.client', '\$event.target'])
-  void onMouseDown(Point client, Element target) {
-    ripple.doRipple(client.x, client.y, target);
-  }
-
-  @HostListener('mouseup')
-  void onMouseUp() {
-    ripple.visible = false;
-    ripple.animating = false;
-  }
-}
-
-@Component(
-    selector: '.mdl-button__ripple-container',
-    template: '<span class="mdl-ripple" [centered]="false"></span>',
-    directives: const [Ripple])
+    template: '<span class="mdl-ripple ng-animate" *ngIf="active"'
+        '[style.width]="rippleWidth" '
+        '[style.height]="rippleHeight" '
+        '[style.top]="rippleY" '
+        '[style.left]="rippleX" '
+        '></span>',
+    styles: const [
+      '.mdl-ripple {transform: translate(-50%, -50%) scale(1);'
+          'transition: all 0.6s cubic-bezier(0.0, 0.0, 0.2, 1.0);}',
+      '.mdl-ripple.ng-enter {opacity: 0;'
+          'transform: translate(-50%, -50%) scale(0.0001);}',
+      '.mdl-ripple.ng-enter-active {opacity: .8;'
+          'transform: translate(-50%, -50%) scale(1);}'
+    ],
+    directives: const [
+      Ripple,
+      NgIf
+    ])
 class RippleContainer {
   @ViewChild(Ripple) Ripple ripple;
+  @Input() bool centered = false;
+  bool active = false;
+  String rippleX, rippleY, rippleWidth, rippleHeight;
 
   @HostListener('mousedown', const ['\$event.client', '\$event.target'])
-  void onMouseDown(Point client, Element target) {
-    ripple.doRipple(client.x, client.y, target);
+  void onMouseDown(Point client, Element ripple) {
+    Rectangle rect = ripple.getBoundingClientRect();
+    int boundHeight = rect.height.toInt();
+    int boundWidth = rect.width.toInt();
+    int rippleSize =
+    (sqrt(boundWidth * boundWidth + boundHeight * boundHeight) * 2 + 2)
+        .toInt();
+    rippleWidth = '${rippleSize}px';
+    rippleHeight = '${rippleSize}px';
+    num x, y;
+    num clientX, clientY;
+    clientX = client.x;
+    clientY = client.y;
+    if (centered) {
+      x = (rect.right - rect.left) / 2;
+      y = (rect.bottom - rect.top) / 2;
+    } else {
+      y = clientY - rect.top;
+      x = clientX - rect.left;
+    }
+    rippleX = '${x}px';
+    rippleY = '${y}px';
+    active = true;
   }
 
   @HostListener('mouseup')
   void onMouseUp() {
-    ripple.visible = false;
-    ripple.animating = false;
+    active = false;
   }
 
   @HostListener('touchstart', const ['\$event.touches[0]', '\$event.target'])
-  void onTouchStart(Point client, Element target) {
-    ripple.doRipple(client.x, client.y, target);
-  }
+  void onTouchStart(Point client, Element target) =>
+      onMouseDown(client, target);
 }
+
+const List ripples = const [Ripple, RippleContainer];
